917017152117f8f45737b1ea9537467b7d41fd2b
Abir <abir@primetechbd.com>
2024-05-09 16:03:41 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import { readFileSync } from "fs";
 import { data } from "./payload";
 
-export const payload = data;
+const payload = data;
+
+const filter = {
+    cabinClasses: ["Y", "B", "E"],
+    fareBrand: "Flexi"
+}
 
 console.log(payload)
 
 let filteredPayload = payload.Payload.filter(x =>
-    x.Onwards.some(y => y.CabinClass === "Y" || y.CabinClass === "B" || y.CabinClass === "E")
+    x.Onwards.some(y => filter.cabinClasses.includes(y.CabinClass))
     &&
-    x.Returns.some(y => y.CabinClass === "Y" || y.CabinClass === "B" || y.CabinClass === "E")
+    x.Returns.some(y => filter.cabinClasses.includes(y.CabinClass))
     &&
-    x.BrandedFareInfoes.map(y => y.FareBrand.toUpperCase().includes("Flexi".toUpperCase()))
+    x.BrandedFareInfoes.map(y => y.FareBrand.toUpperCase().includes(filter.fareBrand.toUpperCase()))
 );
 
 console.log(filteredPayload)
